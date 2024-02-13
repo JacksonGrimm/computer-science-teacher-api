10cacfa34a63a5aac01dd060efb76cbdb87296c9
Jackson Grimm <jacksonGrimm18@gmail.com>
2024-02-12 23:58:26 -0700

    adds express

--- index.ts
+++ index.ts
@@ -1,1 +1,15 @@
-console.log("hello world!");
+import express from "express";
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use("/", (req, res) => {
+  res.send("got it!");
+});
+
+app.listen(PORT, () => {
+  console.log(`API server running on port ${PORT}!`);
+});
