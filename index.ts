4f1b8e67ac7545afb0c27112c0f1bfb22d137aa7
Jackson Grimm <jacksonGrimm18@gmail.com>
2024-02-13 00:38:11 -0700

    implement date check middlware date check is middleware that is route specific

--- index.ts
+++ index.ts
@@ -1,12 +1,14 @@
 import express from "express";
 
+import { dateCheck, dateCheckMiddleware } from "./utils/datecheck.util";
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use("/", (req, res) => {
+app.use("/", dateCheckMiddleware, (req, res) => {
   res.send("got it!");
 });
 
--- utils/datecheck.util.ts
+++ utils/datecheck.util.ts
@@ -1,4 +1,6 @@
-export function dateCheck(date) {
+import { Request, Response, NextFunction } from "express";
+
+export function dateCheck(date: string) {
   const currentDate = new Date();
 
   if (date) {
@@ -6,4 +8,22 @@ export function dateCheck(date) {
   }
 
   return [true, currentDate.toDateString()];
+}
+
+let lastDate: string;
+
+export function dateCheckMiddleware(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const [getNewData, current] = dateCheck(lastDate);
+  if (getNewData && typeof current === "string") {
+    console.log("getting new data");
+    lastDate = current;
+    next();
+    return;
+  } else {
+    console.log("same data");
+  }
 }
